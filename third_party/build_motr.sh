402fed6fb66c187cc4a92aa9b87b1fa6d58a21ec
Rajesh Nambiar <rajesh.nambiar@seagate.com>
2021-06-07 10:04:49 -0600

    EOS-19825: S3 DI PUT related changes with motr changes in place
    Signed-off-by: Rajesh Nambiar <rajesh.nambiar@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -xe
-cd motr
 # Script to build motr.
 # github repo: https://github.com/Seagate/cortx-motr
-# branch:  F23B_DI_PI2_Motr  commit:e5f5e1ae
+# branch: master commit: 1725ba73a4f2b1c8a64c8dab0f2cf8bc8e84c1e2
+cd motr
+git checkout EOS-20955 
 # Uncomment following line to compile motr with both KVS and Cassandra
 # export CONFIGURE_OPTS=--with-cassandra
 PROCESSORS_AVAILABLE=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
