7d8740cc5b4787dad88fb9e5c0e9ffcc153fb189
Nilesh Govande <nilesh.govande@seagate.com>
2021-07-16 02:10:24 -0600

    Advanced motr version for test
    Signed-off-by: Nilesh Govande <nilesh.govande@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -4,7 +4,8 @@
 # branch: master commit: 70701eea1dd1e20fd7c6bdc5bdd1203795d09958
 
 cd motr
-git checkout EOS-20955 
+git checkout tem_s3_int 
+#git checkout EOS-20955 
 # Uncomment following line to compile motr with both KVS and Cassandra
 # export CONFIGURE_OPTS=--with-cassandra
 PROCESSORS_AVAILABLE=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
